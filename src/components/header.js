def221819a8ad6ea05c294a17f8d4824a270fe1d
ShanmugamSakkaravarthy <113248781+ShanmugamSakkaravarthy@users.noreply.github.com>
2023-07-12 10:19:31 +0530

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import 
+const Header = ()=>{
+    return (
+        <div className="headrer">
+            <div className="inleft">
+                <h1>Event Dec<span>or</span></h1>
+            </div>
+            <div className="inright">
+                <h4>Home</h4>
+                <h4>Our Work</h4>
+                <h4>Experience</h4>
+                <h4>contactUs</h4>
+                <h4>SignIn/LoginIn</h4>
+            </div>
+        </div>
+    );
+};
+
+
+export default Header;
